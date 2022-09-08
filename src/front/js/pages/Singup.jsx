7b7c7373f2ef8f02a4bf98c3e9737e81e8a26853
Raquel <raquelmartinc@gmail.com>
2022-09-08 14:34:21 +0000

    primer commit frontend signup

---
+++ src/front/js/pages/Singup.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="container text-center col-5">
+      <main className="form-signin w-100 m-auto">
+        <form>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+          <div className="form-floating">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+
+          <div className="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Sign in
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Signup;
